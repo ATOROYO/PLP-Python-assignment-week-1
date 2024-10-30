1f65921064c2bcd842ec149014a91d734013d267
ATOROYO <davidatoroyosika@gmail.com>
2024-10-30 17:03:19 +0300

    Did question 5

--- user_input.py
+++ user_input.py
@@ -9,3 +9,10 @@ print("Your age is", age)
 # Question 4
 location = input("What is your location?  ")
 print("Your location is", location)
+
+# Question 5
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("What is your location? ")
+
+print("Hello", name, ", you are", age, "years old and live in", location)
